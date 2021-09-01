8a4a8e85e9912b0d6fe4dd58f274dc324c312d5e
wagneroreis <wagner2590@gmail.com>
2021-09-01 12:23:24 -0300

    Primeiro commit

---
+++ Salario com Bônus/salariobonus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso {
+    class Program {
+        static void Main(string[] args)  {
+
+            string nome;
+            double salario, totalv;
+
+            nome = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            totalv = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            totalv = salario + totalv * 0.15;
+
+            Console.WriteLine("TOTAL = R$" + totalv.ToString("F2",CultureInfo.InvariantCulture));
+
+
+
+
+        }
+    }
+}
