99cfe180b86dc4f7c575f220b947800bf5d42766
wagneroreis <wagner2590@gmail.com>
2021-09-01 14:28:03 -0300

    Conversao de Tempo

---
+++ ConversaoDeTempo/tempo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N, horas, minutos, resto, segundos;
+
+            N = int.Parse(Console.ReadLine());
+
+            horas = N / 3600;
+            resto = N % 3600;
+            
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+
+
+
+        }
+    }
+}
