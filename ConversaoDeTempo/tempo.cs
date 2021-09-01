7363ff918d7fb936155cc4e1bb156359a8247959
wagneroreis <wagner2590@gmail.com>
2021-09-01 15:35:05 -0300

    Primeiro Estrutura condicional

--- ConversaoDeTempo/tempo.cs
+++ ConversaoDeTempo/tempo.cs
@@ -7,19 +7,7 @@ namespace Curso
         static void Main(string[] args)
         {
 
-            int N, horas, minutos, resto, segundos;
-
-            N = int.Parse(Console.ReadLine());
-
-            horas = N / 3600;
-            resto = N % 3600;
-            
-            minutos = resto / 60;
-            segundos = resto % 60;
-
-            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
-
-
+            Console.WriteLine("Hello");
 
         }
     }
---
+++ IFeElse/estruturacondicional.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Wagner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        int horas;
+
+        Console.WriteLine("Quantas Horas? ");
+        horas = int.Parse(Console.ReadLine());
+
+        if (horas < 12) {
+            Console.WriteLine("Bom dia!");
+        }
+        else if (horas < 18) {
+                Console.WriteLine("Boa tarde!");
+        }
+        else {
+            Console.WriteLine("Boa noite!");
+        }
+        }
+    }
+}
